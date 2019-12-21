c495f29e51355df2449f69d3dc3c90afda59e88a
kwon12215 <55120756+kwon12215@users.noreply.github.com>
2019-12-21 22:16:33 +0900

    Create Button.py

---
+++ Button.py
@@ -0,0 +1,15 @@
+from PyQt5.QtWidgets import *
+
+
+class Button(QPushButton):
+    ''' PyQt QToolButton
+        Custom Button Class hyunseo jjangjjang'''
+    def __init__(self, image,pos,size, callback=None,parent=None):
+        super().__init__(parent)
+        self.resize(size[0], size[1])
+        self.move(pos[0],pos[1])
+        img_url = 'background-image:url(Images/{});border:0px;'.format(image)
+        self.setStyleSheet(img_url)
+        if callback is not None:
+            self.clicked.connect(callback)
+
